11f4799e72fa6b9b4fc19a90a335dccfa34b79f9
Amol Sharma <amolsharma99@gmail.com>
2013-07-25 01:39:31 +0530

    stata warm up final

--- src/main/java/StatisticsWarmUp.java
+++ src/main/java/StatisticsWarmUp.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,22 +11,69 @@ import java.util.ArrayList;
  */
 public class StatisticsWarmUp {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         // get the arguments and calculate the mean
 
         Integer numOfElements;
+        Double mean = 0.0;
+        Double median;
+        Integer mod = 0;
+        Double standardDeviation = 0.0;
+        Integer[] hashCount = new Integer[100001];
+        hashCount[0] = 0;
         ArrayList<Integer> numList = new ArrayList<Integer>();
-        numOfElements =  Integer.parseInt(args[0]);
-        for(int i=0; i<numOfElements; i++){
-            numList.add(Integer.parseInt(args[i+ 1]));
+        Scanner scan = new Scanner(System.in);
+        numOfElements = scan.nextInt();
+        for (int i = 0; i < numOfElements; i++) {
+            numList.add(scan.nextInt());
         }
 
 
-        for(int i=0; i<numList.size(); i++){
-            System.out.println(numList.get(i));
-
+        for (int i = 0; i < numList.size(); i++) {
+            mean = mean + numList.get(i);
+            if (hashCount[Integer.parseInt(numList.get(i).toString())] == null) {
+                hashCount[Integer.parseInt(numList.get(i).toString())] = 1;
+            } else {
+                hashCount[Integer.parseInt(numList.get(i).toString())]++;
+            }
         }
+
+        mean = mean / numOfElements;
+        Collections.sort(numList);
+        if (numOfElements % 2 != 0) {
+            //middle element
+            median = Double.valueOf(numList.get(numOfElements / 2));
+        } else {
+            median = Double.valueOf(numList.get(numOfElements / 2) + numList.get(numOfElements / 2 - 1));
+            median = median / 2;
+        }
+
+        //mod
+        Integer max = 0;
+        for (Integer i = 0; i < 100001; i++) {
+            if (hashCount[i] != null && hashCount[i] > max) {
+                max = hashCount[i];
+                mod = i;
+            }
+        }
+
+//        standardDeviation
+        for (int i = 0; i < numList.size(); i++) {
+            standardDeviation = standardDeviation + (mean - numList.get(i)) * (mean - numList.get(i));
+        }
+        standardDeviation = standardDeviation / numOfElements;
+        standardDeviation = Math.sqrt(standardDeviation);
+
+        //95% CI
+        Double lowerCI = mean - (1.96 * standardDeviation) / (Math.sqrt(numOfElements));
+        Double upperCI = mean + (1.96 * standardDeviation) / (Math.sqrt(numOfElements));
+
+        System.out.println(mean);
+        System.out.println(median);
+        System.out.println(mod);
+        System.out.println(standardDeviation);
+        System.out.println(lowerCI + " " + upperCI);
 
     }
 
